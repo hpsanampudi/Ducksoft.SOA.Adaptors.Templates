08ce313e53ebcad596df850b4e5841836946227e
hpsanampudi <hpsanampudi@gmail.com>
2018-06-13 18:43:21 +0100

    Added T4 templates for handling build numbers
    Added T4 templates for handling build numbers

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,11 +1,12 @@
-﻿using System.Reflection;
+﻿
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ducksoft.SOA.Adaptors.Templates")]
-[assembly: AssemblyDescription("WCF T4 Templates for adaptors used across SOA application.")]
+[assembly: AssemblyDescription("WCF T4 Templates adaptors used across SOA application.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Duck Software Limited")]
 [assembly: AssemblyProduct("Ducksoft.SOA.Adaptors.Templates")]
@@ -18,18 +19,18 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
 [assembly: Guid("35ab6541-6b73-446b-b3d9-190c21c7ec2e")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      Minor Version - Year
+//      Build Number - MonthDate
+//      Revision - HoursMinutes
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2018.0613.1834")]
+[assembly: AssemblyFileVersion("1.2018.0613.1834")]
