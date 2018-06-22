2fd80bf5963c068ae0b17a4a3db03d4807f4a4ff
hpsanampudi <hpsanampudi@gmail.com>
2018-06-22 16:32:16 +0100

    Auto increment assembly version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM.
-[assembly: Guid("35ab6541-6b73-446b-b3d9-190c21c7ec2e")]
+[assembly: Guid("9f302721-72b1-475e-87c4-48a838a78871")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2018.0613.1834")]
-[assembly: AssemblyFileVersion("1.2018.0613.1834")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+
